1742b8ead55be5b40d94d28adeefdd7e7aea7a8c
mogosbogdan <38261124+mogosbogdan@users.noreply.github.com>
2025-05-28 20:03:18 +0300

    refactor: move PAGE_SIZE and loadOptions function inside App component for better encapsulation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
-const PAGE_SIZE = 10;
-
-const loadOptions = async (search, loadedOptions, { page }) => {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${PAGE_SIZE}`
-  );
-  const posts = await response.json();
-
-  const hasMore = posts.length === PAGE_SIZE;
-
-  const options = posts.map((post) => ({
-    label: post.title,
-    value: post.id,
-  }));
-
-  return {
-    options,
-    hasMore,
-    additional: {
-      page: page + 1,
-    },
-  };
-};
-
-function App() {
+const App = () => {
   const [value, setValue] = useState(null);
+  const PAGE_SIZE = 10;
+
+  const loadOptions = async (search, loadedOptions, { page }) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${PAGE_SIZE}`
+    );
+    const posts = await response.json();
+
+    const hasMore = posts.length === PAGE_SIZE;
+
+    const options = posts.map((post) => ({
+      label: post.title,
+      value: post.id,
+    }));
+
+    return {
+      options,
+      hasMore,
+      additional: {
+        page: page + 1,
+      },
+    };
+  };
 
   return (
     <div style={{ maxWidth: 600, margin: "100px auto" }}>
@@ -41,6 +40,6 @@ function App() {
       />
     </div>
   );
-}
+};
 
 export default App;
